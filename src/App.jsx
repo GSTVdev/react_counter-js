6d212ac302c218962572e173a33cf4d41b349742
danie010830 <danielgukillwar008@gmail.com>
2025-08-22 15:44:50 -0300

    correta

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,32 @@ export const App = () => {
   const [count, setCount] = useState(0);
 
   const addOne = () => {
-    setCount(prev => prev + 1);
+    // write code here
+    setCount(value => value + 1);
   };
 
   const add100 = () => {
-    setCount(prev => prev + 100);
+    // write code here
+    setCount(value => value + 100);
   };
 
-  // Ajustado para passar no teste: se count for 0, soma 1 e depois 100
+  // DON'T change the code below
   const increase = () => {
-    setCount(prev => {
-      const next = prev + 1;
-
-      if (prev === 0 || prev === 100) {
-        return next + 100;
-      }
-
-      if (next % 5 === 0) {
-        return next + 100;
-      }
-
-      return next;
-    });
+    if (count % 5 === 0) {
+      add100();
+    }
+    addOne();
   };
 
   return (
     <div className="App">
       <h1 className="App__title">{`Count: ${count}`}</h1>
-
       <button type="button" className="App__add-one" onClick={addOne}>
         Add 1
       </button>
-
       <button type="button" className="App__add-100" onClick={add100}>
         Add 100
       </button>
-
       <button type="button" className="App__increase" onClick={increase}>
         Increase
       </button>
